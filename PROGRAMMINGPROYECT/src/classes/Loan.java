be2c6b49b608baac9fa94609a7fae0b5c9a3797b
alumno <alumno@10.203.28.60>
2026-02-24 12:01:15 +0100

    second commit

---
+++ PROGRAMMINGPROYECT/src/classes/Loan.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Loan {
+ private String bookCode;
+ private String bookTitle;
+ 
+}
--- PROGRAMMINGPROYECT/src/classes/User.java
+++ PROGRAMMINGPROYECT/src/classes/User.java
@@ -2,6 +2,8 @@ package classes;
 
 import java.time.LocalDate;
 
+import exceptions.InvalidUserException;
+
 public class User {
 	private String name;
 	private String email;
@@ -11,14 +13,39 @@ public class User {
 	private LocalDate sanctionEndDate;
 
 	public User(String name, String email, String memberNumber, LocalDate registrationDate, boolean sanctioned,
-			LocalDate sanctionEndDate) {
-		super();
+			LocalDate sanctionEndDate) throws InvalidUserException {
+
+		if (!email.contains("@") || !email.contains(".")) {
+			throw new InvalidUserException("Invalid email format.");
+		}
+		if (!memberNumber.matches("SOC\\d{5}")) {
+			throw new InvalidUserException("Invalid member number.");
+		}
 		this.name = name;
 		this.email = email;
 		this.memberNumber = memberNumber;
 		this.registrationDate = registrationDate;
-		this.sanctioned = sanctioned;
-		this.sanctionEndDate = sanctionEndDate;
+		this.sanctioned = false;
+		this.sanctionEndDate = null;
 	}
-	
+
+	public void sanction(int days) {
+		this.sanctioned = true;
+		this.sanctionEndDate = LocalDate.now().plusDays(days);
+	}
+
+	public void liftSanction() {
+		this.sanctioned = false;
+	}
+
+	public boolean isSancioned() {
+		return sanctioned;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", email=" + email + ", memberNumber=" + memberNumber + ", registrationDate="
+				+ registrationDate + ", sanctioned=" + sanctioned + ", sanctionEndDate=" + sanctionEndDate + "]";
+	}
+
 }
